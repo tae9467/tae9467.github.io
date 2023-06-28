a872e5e4c2ae732bcf65d4c6454a36ffbdedc694
Toula Elwell <toula.elwell@qualitrolcorp.com>
2023-06-28 16:34:19 -0400

    added a comment to my javascript function

--- Functionality/script.js
+++ Functionality/script.js
@@ -1,3 +1,9 @@
+/**
+    * Opens the navigation menu
+    * @function toggleMobileMenu
+    * @param {string} menu - a class that gets put onto the mobile menu
+    * @returns {void}
+*/
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
