d96758e1e1c2fd4fee1635e7e39d0f797d4e57ae
tae9467 <tae9467@g.rit.edu>
2023-07-21 13:50:18 -0400

    slowly updating the header so that my website looks more modern, working on the footer next

--- Functionality/script.js
+++ Functionality/script.js
@@ -7,3 +7,27 @@
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
+
+/**
+    * Turns all three divs (bars) border color in the the mobile menu pink simultaniously when hovering over one bar
+    * @function turnPink
+    * @returns {void}
+*/
+function turnPink() {
+    const bars = document.querySelectorAll('.bar');
+    bars.forEach(bar => {
+      bar.style.borderColor = 'hsl(330, 45%, 65%)';
+    });
+}
+
+/**
+    * Turns all three divs (bars) border color in the the mobile menu back to the original color simultaniously when the mouse isn't hovering over a bar
+    * @function resetColor
+    * @returns {void}
+*/
+function resetColor() {
+    const bars = document.querySelectorAll('.bar');
+    bars.forEach(bar => {
+      bar.style.borderColor = 'hsl(240, 3%, 88%)';
+    });
+}
